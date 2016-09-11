36543d9be36e54878b2a84a9bf715e3d1814b331
Aviral Shukla <hot.avishk96@gmail.com>
2016-09-11 13:04:35 +0530

    modified index file

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
-<!DOCTYPE html>
+        <!DOCTYPE html>
 <html>
 <head>
-     <title></title>
+     <title>My First Task</title>
      </head>
      <body>
          <p>Hello World</p>
+         <p> SEcond p </p>
          </body>
          </html>
