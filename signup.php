5b32e915d1da2eb6eb7e97cee776db8052031b26
Aviral Shukla <hot.avishk96@gmail.com>
2016-09-16 18:39:00 +0530

    my 2nd commit

--- signup.php
+++ signup.php
@@ -8,21 +8,23 @@
      
  
      </head>
-     <body>
-        <div class="container">
+  <body>
+     <div class="container">
         <div class="jumbotron">
-           <h2 id="header" class="loginForm">Login Form</h2>
-           </div>
+           <h2 id="header" class="loginForm">Signup Form</h2>
+         </div>
            <div class="row">
            <div class="col-md-4"></div>
            <div class="col-md-4">
-           <p class="text-center"> In publishing and graphic design, lorem ipsum (derived from Latin dolorem ipsum, translated as "pain itself") is a filler text commonly used to demonstrate the graphic elements of a document or visual presentation. Replacing meaningful content with placeholder text allows designers to design the form of the content before the content itself has been produced.[citation needed]
+           <p class="text-center"> Signup</p>
            <div class="col-md-4"></div>
-      <form name="loginForm">
+      <form name="Signup Form">
     <div class="form-group">
     <label for="email" >Email</label>
     <input type="text" id="email" class="loginForm"/></br>
+     <div class="col-md-4"></div>
     <label for="password">Password</label>
+
      <input type="password" id="password" class="loginForm" /></br>
     <input type="submit" value="Login" >
 </form>
